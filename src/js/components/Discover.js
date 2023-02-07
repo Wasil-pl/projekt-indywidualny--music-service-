e92268d82b9571deaa40115d13a6707cb3a522fe
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-02-07 19:16:45 +0100

    add discover function

---
+++ src/js/components/Discover.js
@@ -0,0 +1,65 @@
+import { classNames, select, templates } from '../settings.js';
+import SongPlayer from './SongPlayer.js';
+import utils from '../utils.js';
+
+class Discover {
+  constructor(songList) {
+    const thisDiscover = this;
+
+    thisDiscover.songList = songList;
+    thisDiscover.getElements();
+    thisDiscover.renderInMenu();
+    thisDiscover.initSongs();
+    thisDiscover.randomSong();
+  }
+
+  getElements() {
+    const thisDiscover = this;
+
+    thisDiscover.dom = {
+      discoverContainer: document.querySelector(select.containerOf.discover),
+    };
+
+    thisDiscover.songs = thisDiscover.dom.discoverContainer.getElementsByTagName(select.all.article);
+  }
+
+  renderInMenu() {
+    const thisDiscover = this;
+
+    const generatedHTML = templates.discover();
+    thisDiscover.element = utils.createDOMFromHTML(generatedHTML);
+    thisDiscover.dom.discoverContainer.appendChild(thisDiscover.element);
+  }
+
+  initSongs() {
+    const thisDiscover = this;
+
+    for (let song in thisDiscover.songList){
+      new SongPlayer (thisDiscover.songList[song].id, thisDiscover.songList[song], thisDiscover.dom.discoverContainer);
+    }
+
+    const songListId = [];
+
+    for (let song of thisDiscover.songs) {
+      const songId = song.getAttribute('song-id');
+      songListId.push(songId);
+      song.classList.add(classNames.discover.hidden);
+    }
+
+    thisDiscover.randomSongId = songListId[Math.floor(Math.random() * songListId.length)];
+  }
+
+  randomSong() {
+    const thisDiscover = this;
+
+    for (let song of thisDiscover.songs) {
+      const songId = song.getAttribute('song-id');
+      if ( songId === thisDiscover.randomSongId) {
+        song.classList.remove(classNames.discover.hidden);
+      }
+    }
+
+  }
+}
+
+export default Discover;

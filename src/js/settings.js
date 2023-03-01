67f084840b957cacd51dff0832b0ede1abac3c6f
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-03-01 18:01:19 +0100

    Revert "add server.mjs"
    This reverts commit 0b58273edf370439c33821e49b40da1390279c14.

--- src/js/settings.js
+++ src/js/settings.js
@@ -38,7 +38,7 @@ export const classNames = {
 
 export const settings = {
   db: {
-    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    url: '//localhost:3131',
     songs: 'songs',
   },
 };

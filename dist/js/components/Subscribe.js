15d5a3b63fc6abb2be9be60134449ce67e35dd3b
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-03-01 18:33:44 +0100

    add dist

---
+++ dist/js/components/Subscribe.js
@@ -0,0 +1,22 @@
+import { select, templates } from '../settings.js';
+import utils from '../utils.js';
+
+class Subscribe {
+  constructor() {
+    const thisSubscribe = this;
+
+    thisSubscribe.renderInMenu();
+  }
+
+  renderInMenu() {
+    const thisSubscribe = this;
+
+    const generatedHTML = templates.subscribe();
+    thisSubscribe.element = utils.createDOMFromHTML(generatedHTML);
+    const menuContainer = document.querySelector(select.containerOf.home);
+    menuContainer.appendChild(thisSubscribe.element);
+
+  }
+}
+
+export default Subscribe;
---
+++ dist/js/components/search.js
@@ -0,0 +1,94 @@
+import { CATEGORIES_SEPARATOR, classNames, select, templates } from '../settings.js';
+import SongPlayer from './SongPlayer.js';
+import utils from '../utils.js';
+
+class Search {
+  constructor(songList, categoriesArray) {
+    const thisSearch = this;
+
+    thisSearch.songList = songList;
+    thisSearch.categoriesArray = categoriesArray;
+
+    thisSearch.getElements();
+    thisSearch.renderInMenu();
+    thisSearch.initSongs();
+    thisSearch.searchSong();
+  }
+
+  getElements() {
+    const thisSearch = this;
+
+    thisSearch.dom = {
+      songsContainer: document.querySelector(select.containerOf.songs),
+      searchContainer: document.querySelector(select.containerOf.search),
+    };
+
+    thisSearch.songs = thisSearch.dom.songsContainer.getElementsByTagName(select.all.article);
+  }
+
+  renderInMenu() {
+    const thisSearch = this;
+
+    const generatedHTML = templates.search(thisSearch.categoriesArray);
+    thisSearch.element = utils.createDOMFromHTML(generatedHTML);
+    thisSearch.dom.searchContainer.appendChild(thisSearch.element);
+  }
+
+  initSongs() {
+    const thisSearch = this;
+
+    for (let song in thisSearch.songList){
+      new SongPlayer (thisSearch.songList[song], thisSearch.dom.songsContainer);
+    }
+
+    for ( let song of thisSearch.songs ) {
+      song.classList.add(classNames.hidden);
+    }
+  }
+
+  searchSong() {
+    const thisSearch = this;
+
+    const searchBtn = document.querySelector(select.all.searchBtn);
+    const inputValue = document.getElementById('myInput');
+    const selectCategory = document.getElementById('myCategories');
+    const txtFoundSongs = document.querySelector(select.containerOf.numberOfSongsTxt);
+
+    searchBtn.addEventListener('click', function(e){
+      e.preventDefault();
+      const searchValue = inputValue.value.toLowerCase();
+      const selectedCategoryValue = selectCategory.value.toLowerCase();
+      const numberOfSongsArray = [];
+
+      for (let song of thisSearch.songs){
+        const authorAndTitle = song.getAttribute('author-title').toLowerCase();
+        const songCategories = song.querySelector(select.containerOf.songCategories).innerHTML.toLowerCase().split(CATEGORIES_SEPARATOR);
+
+        song.classList.add(classNames.hidden);
+
+        if (!selectedCategoryValue && Boolean(searchValue) && authorAndTitle.includes(searchValue)) {
+          song.classList.remove(classNames.hidden);
+          numberOfSongsArray.push(song);
+
+        } else if (Boolean(searchValue) && authorAndTitle.includes(searchValue) && songCategories.includes(selectedCategoryValue)) {
+          song.classList.remove(classNames.hidden);
+          numberOfSongsArray.push(song);
+        }
+      }
+
+      txtFoundSongs.innerHTML = '';
+
+      if (numberOfSongsArray.length === 1) {
+        const songsFoundHtml = '<p>We have found ' + numberOfSongsArray.length + ' song</p>';
+        txtFoundSongs.insertAdjacentHTML('beforeend', songsFoundHtml);
+      }
+
+      else {
+        const songsFoundHtml = '<p>We have found ' + numberOfSongsArray.length + ' songs</p>';
+        txtFoundSongs.insertAdjacentHTML('beforeend', songsFoundHtml);
+      }
+    });
+  }
+}
+
+export default Search;
